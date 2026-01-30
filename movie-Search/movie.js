b62cfea1d996668d0a4f66ed0cffce51b6f3aa6e
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-01-30 10:28:51 +0430

    changes

--- movie-Search/movie.js
+++ movie-Search/movie.js
@@ -6,3 +6,17 @@ const horrordiv = document.getElementById("horror");
 const romantictiv = document.getElementById("romantic");
 const spiderdiv = document.getElementById("spider");
 const comedydiv = document.getElementById("comedy");
+const input = document.getElementById("input");
+hbtn.addEventListener("click",()=>{
+    if(horrordiv.classList.contains("hidden")){
+        horrordiv.classList.remove("hidden");
+        horrordiv.classList.add("flex");
+        romantictiv.classList.remove("flex");
+        romantictiv.classList.add("hidden");
+        spiderdiv.classList.remove("flex");
+        spiderdiv.classList.add("hidden")
+        comedydiv.classList.remove("flex");
+        comedydiv.classList.add("hidden");
+        input.innerText = "horror";
+    }
+})
